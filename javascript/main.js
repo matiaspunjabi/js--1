5a3a7d42cb08dfe313baf6c0c14ff227d124a376
matias <matiaspun92@gmail.com>
2023-02-25 12:35:25 -0300

    play and pause audio from the html

--- javascript/main.js
+++ javascript/main.js
@@ -1,27 +1,29 @@
-// Music player
+//*******************  Music player *******************
 
-//[] play a mp3 file from navigator.
+//[x] play a mp3 file from navigator.
 //[] functional next and prev song.
 //[] functional shuffle and repeat buttons.
 //[] add and remove songs to likesongs playlist.
 //[] create a personal playlist.
 //[] add an own mp3 file to music player.
 //[] css responsive, favicon customize asset
-//[]
-//[]
-//[]
-//[]
 
 
+//-------------------------------------------- 
 
 
+let linkin = new Audio("../songs/music-3.mp3");
 
+const btn = document.querySelector(".play");
 
+btn.addEventListener("click", ()=>{
+    linkin.play();
+})
 
-
-
-
-
+const btnPause = document.querySelector(".pause");
+btnPause.addEventListener("click", ()=>{
+    linkin.pause();
+})
 
 
 
