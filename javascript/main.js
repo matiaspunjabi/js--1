f1d3ff55ea11f3d0fdf1c7ab200de67ab36f4248
matias <matiaspun92@gmail.com>
2023-02-25 11:22:39 -0300

    starting prepoject3

---
+++ javascript/main.js
@@ -0,0 +1,36 @@
+// Music player
+
+//[] play a mp3 file from navigator.
+//[] functional next and prev song.
+//[] functional shuffle and repeat buttons.
+//[] add and remove songs to likesongs playlist.
+//[] create a personal playlist.
+//[] add an own mp3 file to music player.
+//[] css responsive, favicon customize asset
+//[]
+//[]
+//[]
+//[]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    
+
+
+
+
